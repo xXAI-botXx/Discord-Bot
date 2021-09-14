f9d8d933121b4bd2743f8dad90a5862c3d9288f2
Tobia Ippolito <tobiaippolito@outlook.de>
2021-09-14 16:19:55 +0200

    small change

--- main.py
+++ main.py
@@ -240,8 +240,7 @@ class Bot_xX_Destroyer_Xx(discord.Client):
         elif message.content.lower() in ["münze", "coin", "münzwurf", "coinflip"]:
             txt = "Ok ich werfe eine Münze..."
             await message.channel.send(txt)
-            time.sleep(1)
-            txt = f"{random.choice(['Kopf', 'Zahl'])}"
+            txt = f">>> {random.choice(['Kopf', 'Zahl'])} <<<"
             await message.channel.send(txt)
 
     async def greeting_event(self, message):
